fe50dd2d3262e3b5fc3a632afe9c90598e0ac017
ivangv0505 <ivan_garrido51@outlook.com>
2024-01-13 23:11:13 -0600

    Cifrado de Hill y Determinantes

---
+++ determinantes_cifrado/cifradohill.py
@@ -0,0 +1,49 @@
+import numpy as np
+from sympy import Matrix
+
+#Matriz clave
+clave = np.array([[3, 3], [2, 5]])
+
+#Inversa de la matriz clave
+clave_inversa = np.array(Matrix(clave).inv_mod(28))
+
+def encriptar_mensaje(clave, mensaje):
+    tabla = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19, 'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25, ' ': 26, 'Ñ': 27}
+
+    numeros = [tabla[letra.upper()] for letra in mensaje]
+
+    while len(numeros) % clave.shape[0] != 0:
+        numeros.append(26)
+
+    matriz = np.array(numeros).reshape(-1, clave.shape[0])
+    resultado = np.dot(matriz, clave)
+    resultado = resultado % 28
+    letras = ''.join([list(tabla.keys())[list(tabla.values()).index(int(numero))] for numero in resultado.flatten()])
+
+    return letras
+
+def desencriptar_mensaje(clave, clave_inversa, mensaje_cifrado):
+    tabla = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19, 'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25, ' ': 26, 'Ñ': 27}
+
+    numeros = [tabla[letra.upper()] for letra in mensaje_cifrado]
+    
+    matriz = np.array(numeros).reshape(-1, clave.shape[0])
+    resultado = np.dot(matriz, clave_inversa)
+    resultado = resultado % 28
+    letras = ''.join([list(tabla.keys())[list(tabla.values()).index(int(numero))] for numero in resultado.flatten()])
+
+    return letras
+
+def imprimirEncriptacion(clave): 
+    mensaje_original = input("Ingresa la palabra o frase que deseas encriptar: ")
+    mensaje_encriptado = encriptar_mensaje(clave, mensaje_original)
+    print(f"La palabra o frase encriptada es: {mensaje_encriptado}")
+
+def imprimirDesencriptacion(clave, clave_inversa):
+    mensaje_cifrado = input("Ingresa el mensaje cifrado que deseas desencriptar: ")
+    mensaje_desencriptado = desencriptar_mensaje(clave, clave_inversa, mensaje_cifrado)
+    print(f"La palabra o frase desencriptada es: {mensaje_desencriptado}")
+
+imprimirEncriptacion(clave)
+
+imprimirDesencriptacion(clave, clave_inversa)
---
+++ determinantes_cifrado/cofactores.py
@@ -0,0 +1,75 @@
+#Determinantes (método de cofactores(laplace))
+# + - +
+# - + -   ---> Ejemplo de una matriz 3*3
+# + - +
+import numpy as np
+
+def cofactor(matriz, fila, columna):
+    submatriz = np.delete(np.delete(matriz, fila, axis=0), columna, axis=1)
+    print(f"\nCalculando el cofactor para el elemento de la fila {fila + 1}, columna {columna + 1}:\n{submatriz}")
+
+    # Obtener los elementos de la submatriz para la explicación
+    elementos_submatriz = submatriz.flatten()
+
+    # Mostrar la operación específica para calcular el cofactor
+    operacion = f"{elementos_submatriz[0]} * {elementos_submatriz[3]} - {elementos_submatriz[1]} * {elementos_submatriz[2]}"
+    print(f"Operación para el cofactor: {operacion}")
+
+    cof = ((-1) ** (fila + columna)) * determinante(submatriz)
+    print(f"Resultado del cofactor: {cof}")
+    return cof
+
+def determinante(matriz):
+    size = len(matriz)
+
+    if size == 1:
+        return matriz[0, 0]
+    elif size == 2:
+        return matriz[0, 0] * matriz[1, 1] - matriz[0, 1] * matriz[1, 0]
+    else:
+        det = 0
+        expresion_cofactores = ""
+        for columna in range(size):
+            cof = cofactor(matriz, 0, columna)
+            print(f"\nMultiplicando el elemento en la fila 1, columna {columna + 1} por su cofactor:")
+            print(f"{matriz[0, columna]} * {cof} = {matriz[0, columna] * cof}")
+            if columna == 0:
+                expresion_cofactores += f"{matriz[0, columna]} * {cof} "
+            else:
+                expresion_cofactores += f"+ {matriz[0, columna]} * {cof} "
+            det += matriz[0, columna] * cof
+
+        #expresion_cofactores = expresion_cofactores[:-2]  # Eliminar el último "+ "
+        print(f"\nExpresión de la suma de los cofactores: {expresion_cofactores}")
+        print(f"Suma: {det}")
+        return det
+
+def insertar_matriz():
+    try:
+        fila = int(input("Ingresa el número de filas de la matriz (máximo 5): "))
+        columna = int(input("Ingresa el número de columnas (máximo 5): "))
+        if fila > 5 or columna > 5:
+            raise ValueError("Error, límite superado. Valores fuera del rango.")
+        elif fila < 1 or columna < 1:
+            raise ValueError("No puedes ingresar datos menores a 1.")
+        elif fila != columna:
+            raise ValueError("Error, la matriz debe ser cuadrada. Ingresa el mismo número de filas y columnas")
+        matriz = np.zeros((fila, columna))
+
+        for i in range(fila):
+            for j in range(columna):
+                matriz[i, j] = float(input(f"Ingrese el elemento en la posición ({i + 1}, {j + 1}): "))
+        print("\nMatriz ingresada:")
+        print(matriz)
+        return matriz
+    except ValueError as error:
+        print(f"Error: {error}")
+        return None
+
+if __name__ == "__main__":
+    matriz_usuario = insertar_matriz()
+
+    if matriz_usuario is not None:
+        print("\nCalculando el determinante usando el método de cofactores (laplace)...")
+        resultado = determinante(matriz_usuario)
+        print(f"\nDeterminante por cofactores: {resultado}")
